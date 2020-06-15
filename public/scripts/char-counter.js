ff66bc1f660de5c96e15501113f9f12f567972ad
Maribelle <maribelleleongzz@gmail.com>
2020-06-15 17:15:27 -0300

    added jQuery to change counter and its color

---
+++ public/scripts/char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    const tweetText = $('#tweet-text');
+    const counter = $('.counter');
+
+    tweetText.on('input', function () {
+        const counterVal = 140 - tweetText.val().length;
+        counter.text(counterVal);
+
+        if (counterVal < 0) {
+            counter.addClass('red-text');
+        } else {
+            counter.removeClass('red-text');
+
+        }
+
+    })
+});
